da832896e99b97aa9899ef363a05e373bc428332
tejasri757 <tejasrikomanduri804@gmail.com>
2020-12-08 12:16:58 +0530

    Add project files.

---
+++ WebApplication1/Home.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+namespace WebApplication1
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            //Coneection String by default empty  
+            string ConStr = "";
+            //Extantion of the file upload control saving into ext because   
+            //there are two types of extation .xls and .xlsx of Excel   
+            string ext = Path.GetExtension(FileUpload1.FileName).ToLower();
+            //getting the path of the file   
+            string path = Server.MapPath("~/Users/ukomanduri/OneDrive/" + FileUpload1.FileName);
+            //saving the file inside the MyFolder of the server  
+            FileUpload1.SaveAs(path);
+            Label1.Text = FileUpload1.FileName + "\'s Data showing into the GridView";
+            //checking that extantion is .xls or .xlsx  
+            if (ext.Trim() == ".xls")
+            {
+                //connection string for that file which extantion is .xls  
+                ConStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+            }
+            else if (ext.Trim() == ".xlsx")
+            {
+                //connection string for that file which extantion is .xlsx  
+                ConStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+            }
+            //making query  
+            string query = "SELECT * FROM [Sheet1$]";
+            //Providing connection  
+            OleDbConnection conn = new OleDbConnection(ConStr);
+            //checking that connection state is closed or not if closed the   
+            //open the connection  
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            //create command object  
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            // create a data adapter and get the data into dataadapter  
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            //fill the Excel data to data set  
+            da.Fill(ds);
+            //set data source of the grid view  
+            gvExcelFile.DataSource = ds.Tables[0];
+            //binding the gridview  
+            gvExcelFile.DataBind();
+            //close the connection  
+            conn.Close();
+        }
+    }
+}
